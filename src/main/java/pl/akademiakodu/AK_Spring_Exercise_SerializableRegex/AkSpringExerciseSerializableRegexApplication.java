546be4945b0f97d47d1ad8e7ba21b558deee1409
Kerawos <Maresowa@gmail.com>
2017-11-21 10:56:28 +0100

    Initial commit

---
+++ src/main/java/pl/akademiakodu/AK_Spring_Exercise_SerializableRegex/AkSpringExerciseSerializableRegexApplication.java
@@ -0,0 +1,12 @@
+package pl.akademiakodu.AK_Spring_Exercise_SerializableRegex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkSpringExerciseSerializableRegexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkSpringExerciseSerializableRegexApplication.class, args);
+	}
+}
